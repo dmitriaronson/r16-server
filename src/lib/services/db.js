5e104bf3d48087f626e1b269c148715dbef20732
Dmitri Perepelenko <dmitri.perepelenko@moneyfarm.com>
2017-02-14 17:05:59 +0000

    update mongoose to use native promise

--- src/lib/services/db.js
+++ src/lib/services/db.js
@@ -4,6 +4,8 @@ import logger from './logger';
 
 const db = mongoose.connection;
 
+mongoose.Promise = global.Promise;
+
 db.on('error', error => logger.error(error));
 db.on('close', () => logger.info('Database connection closed.'));
 db.once('open', () => logger.info(`Connected to ${nconf.get('DB_URI')}`));
