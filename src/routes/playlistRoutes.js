bca513a5af297707dbde2c399bb503a2267d6906
Dmitri Perepelenko <dmitri.perepelenko@moneyfarm.com>
2017-02-14 17:57:57 +0000

    playlist endpoints cleanup

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -1,21 +1,52 @@
 import Router from 'koa-router';
+import uuid from 'uuid';
+import Boom from 'boom';
+import Playlist from '../lib/models/Playlist';
+import { validatePlaylist } from '../lib/services/validate';
+import logger from '../lib/services/logger';
 
-const router = new Router({ prefix: '/p' });
+const router = new Router({ prefix: '/api/playlist' });
 
-router.get('/:playlistId', (ctx) => {
-  ctx.body = 'Hello World!';
+router.get('/', async (ctx) => {
+  const playlists = await Playlist.find({});
+
+  ctx.body = { data: playlists };
 });
 
-router.post('/new', (ctx) => {
+router.get('/:playlistId', async (ctx) => {
+  const id = ctx.params.playlistId;
+  const playlist = await Playlist.findOne({ _id: id });
 
+  ctx.body = { data: playlist };
 });
 
-router.put('/:playlistId', function (ctx) {
+router.post('/', async (ctx) => {
+  const body = ctx.request.body;
 
-});
+  try {
+    await validatePlaylist(body);
 
-router.del('/:playlistId', function (ctx) {
+    const { title, description, tracks } = body;
 
+    const playlist = new Playlist({
+      id: uuid.v4(),
+      author: 'ctx.session.passport.user._id',
+      title,
+      description,
+      tracks,
+    });
+
+    const reply = await playlist.save();
+
+    ctx.body = { data: reply.id };
+  } catch (e) {
+    logger.error(e);
+
+    if (e.name === 'ValidationError') {
+      ctx.status = 400;
+      ctx.body = Boom.badRequest(e.message).output;
+    }
+  }
 });
 
 export default router;
